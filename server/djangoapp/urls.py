1fbc45f6834ee0196431f4672c899742432f8d70
FelixFunesFS <envision@mkqconsulting.com>
2025-12-15 17:49:41 -0500

    Add user registration functionality and Register page routing

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,19 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
 
-app_name = 'djangoapp'
+from . import views
+
+app_name = "djangoapp"
+
 urlpatterns = [
-    # # path for registration
-
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
+    path("login", views.login_user, name="login"),
+    path("logout", views.logout_user, name="logout"),
+    path("register", views.registration, name="register"),
 
     # path for dealer reviews view
+    # path("dealer/<int:dealer_id>/reviews", views.get_dealer_reviews, name="dealer_reviews"),
 
     # path for add a review view
-
+    # path("review", views.add_review, name="add_review"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
